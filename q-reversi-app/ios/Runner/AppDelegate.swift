aac7a0b7d9366757459d4219a9f2f1511e4131cf
Kenta Suzuki_Mac <sken1220@gmail.com>
2026-08-13 14:05:20 +0900

    ios appCheck modification

--- q-reversi-app/ios/Runner/AppDelegate.swift
+++ q-reversi-app/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import FirebaseAppCheck
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,9 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    #if DEBUG
+    AppCheck.setAppCheckProviderFactory(AppCheckDebugProviderFactory())
+    #endif
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
